4f245065d01786811741f5f351c34c0313d72f4e
Nguyen van sang <nguyenvansang30102001@gmail.com>
2021-01-16 21:23:19 +0700

    chay duoc

--- sildebaar/jscript/js.js
+++ sildebaar/jscript/js.js
@@ -1,39 +1,40 @@
-let total =0;
-let tax=0;
-let pay=0;
+var total = 0;
+var tax = 0;
+var pay = 0;
 
-function add_cart(id, name, price)
-{
-  let check=document.getElementsByClassName(id);
-  if(check.length ==0)
-  {
+function add_cart(id, name, price) {
+  var check = document.getElementsByClassName(id);
+  
+  if(check.length == 0){
+    total = total + price;
+    tax = total/100 * 10;
+    pay = total + tax;
     
-      total+= price;
-      tax=total/100*10;
-      pay=total+tax;
-      document.getElementById("total").innerHTML=total;
-      document.getElementById("tax").innerHTML=tax;
-      document.getElementById("payment").innerHTML=pay;
-      let html=document.createElement('div');
-      html.className=id;
-      html.innerHTML+='<p>'+name +"</p>";
-      html.innerHTML+='<p>'+price +"</p>";
-      document.getElementById("list-order").appendChild(html);
+    document.getElementById('total').innerHTML = total;
+    document.getElementById('tax').innerHTML = tax;
+    document.getElementById('payment').innerHTML = pay;
+    
+    var html = document.createElement('div');
+        html.className = id;
+        html.innerHTML += '<p>'+name+'</p>';
+        html.innerHTML += '<p>'+price+'</p>';
+    
+    document.getElementById('list-order').appendChild(html);
   }
-    
 }
-// function remove_item(id, price)
-// {
-//   let check=document.getElementsByClassName(id);
-//   if(check.length>0)
-//   {
-//     total-=price;
-//     tax=total/100*10;
-//     pay=total+tax;
-//     document.getElementById("total").innerHTML=total;
-//     document.getElementById("tax").innerHTML=tax;
-//     document.getElementById("payment").innerHTML=pay;
 
-//   }
-
-// }
+function remove_item(id, price) {
+  var check = document.getElementsByClassName(id);
+  
+  if(check.length > 0){
+    total = total - price;
+    tax = total / 100 * 10;
+    pay = total + tax;
+    
+    document.getElementById('total').innerHTML = total;
+    document.getElementById('tax').innerHTML = tax;
+    document.getElementById('payment').innerHTML = pay;
+    
+    check[0].remove();
+  }
+}
