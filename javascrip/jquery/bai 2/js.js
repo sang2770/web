a52d17bb58cbdcc2f95f6fad88c6131d5762cc86
Nguyen van sang <nguyenvansang30102001@gmail.com>
2021-01-18 21:42:22 +0700

    them bai 3

--- javascrip/jquery/bai 2/js.js
+++ javascrip/jquery/bai 2/js.js
@@ -1,16 +1,10 @@
 $(function () {
-    //cho tat ca noi dung thu lại
     $(".noidung").slideUp();
-    // chọn thẻ h3
-    $(".khoi h3").click(function (e) { 
-        e.preventDefault();
-        console.log("da click");
-        // $(this).next().slideDown();
-        // $(".noidung").slideUp();
-        $(this).next().slideToggle();// gộp của slidedown và slide up
-        // $(this).addClass('xanh');//them class\
-        $(this).toggleClass('xanh'); //cả thêm cả xóa class
-
-    });
+    $(".khoi h3").click(function(e)
+    {
+        console.log("Da clidk");
+        $(this).next().slideToggle();
+        $(this).addClass('xanh');
+    })
 
 });
---
+++ javascrip/jquery/bai 3/js.js
@@ -0,0 +1,25 @@
+$(function() {
+    console.log("Code dang chay!");
+    //tinh vi tri
+    // console.log($(".chapter1").offset().top)
+    $('.menu ul li:nth-child(1) a').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(".chapter1").offset().top}, 1400, "easeOutExpo");
+    });
+    $('.menu ul li:nth-child(2) a').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(".chapter2").offset().top}, 1400, "easeOutBounce");
+    });
+    $('.menu ul li:nth-child(3) a').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(".chapter3").offset().top}, 1400, "easeOutQuint");
+    });
+    $('.menu ul li:nth-child(4) a').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(".chapter4").offset().top}, 1400);
+    });
+    $('.menu ul li:nth-child(5) a').on('click', function (event) {
+        event.preventDefault();
+        $('html,body').animate({scrollTop:0}, 1400);
+    });
+});
