638294abda35776ce9b77354891fbb3c291af830
Nguyen van sang <nguyenvansang30102001@gmail.com>
2021-01-24 11:29:51 +0700

    them web animation

--- javascrip/jquery/webtest2/js.js
+++ javascrip/jquery/webtest2/js.js
@@ -5,9 +5,12 @@ $(function () {
         $(".nen").removeClass("new");
         clearInterval(thongbao);
     },7000);
-
+    $(".nen").click(function (e) { 
+        e.preventDefault();
+        $(".nen").removeClass("new");
+    });
     $(".menu ul li:nth-child(1)").on('click', function (event) {
         event.preventDefault();
-        $("html, body").animate({scrollTop:$(".chapter1").offset().top-100}, 1400);
+        $("html, body").animate({scrollTop:$(".chapter1").offset().top-80}, 1000);
     });
 });
